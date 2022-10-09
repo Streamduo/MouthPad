252332211fe0747374303e6be38675ddc300ec24
FuDuo <duoendeacor@163.com>
2022-10-09 20:44:24 +0800

    七次修改问题

--- app/src/main/java/com/mouth/pad/adapter/MaterialRequisitionStuffSmallListAdapter.kt
+++ app/src/main/java/com/mouth/pad/adapter/MaterialRequisitionStuffSmallListAdapter.kt
@@ -15,7 +15,6 @@ class MaterialRequisitionStuffSmallListAdapter(layoutResId: Int) :
         item.apply {
             holder.setText(R.id.te_material, materialName)
             holder.setText(R.id.te_specification, model)
-            holder.setText(R.id.te_physical_inventory, realStockNum)
             holder.setText(R.id.te_quantity, consumeNum)
             holder.setText(R.id.te_available_stock, realStockNum)
             holder.setText(R.id.te_unit, unit)
